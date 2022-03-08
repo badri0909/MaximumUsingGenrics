cf988516379d2d992262887eadbe3a4c84ccdf96
badri0909 <singuveerabhadrarao@gmail.com>
2022-03-08 18:22:25 +0530

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MaxProblemUsingGenerics
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Maximum Number USing Generics\n");
+
+            //IntegerMax integerMax = new IntegerMax();
+            //integerMax.MaximumInteger(2, 6, 4);
+
+            //FloatMax floatMax = new FloatMax();
+            //floatMax.MaximumFloatNum(6.8,3.9,18.9);
+
+            //StringMax stringMax = new StringMax();
+            //stringMax.MaximumString("Apple", "Peach", "Zebra");
+
+
+
+
+        }
+    }
+}
